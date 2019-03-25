61cc0ac584e0947e4c220e393c9a099164bf39ad
dtgritman <dtgritman@gmail.com>
2019-03-25 01:29:31 -0400

    Added functionality to control rc car with ps4 controller (will add gun support soon). Also will handle ctrl+c to exit and stop input

--- code/controller.c
+++ code/controller.c
@@ -1,14 +1,24 @@
-#include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <signal.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <time.h>
 
 #include <pigpiod_if2.h>
 
-
+//Motor ESC Values
 #define MOTOR_ESC 12
+#define MOTOR_ESC_MIN 1000
+#define MOTOR_ESC_MAX 2000
+#define MOTOR_DEADZONE 12
+
+//Steering Servo Values
 #define SERVO_STEERING 13
+#define SERVO_STEERING_MIN 820
+#define SERVO_STEERING_MAX 1480
 
 //Controller Code Types
 #define EV_KEY 1
@@ -58,60 +68,127 @@ struct controller_status {
     unsigned short js_x;
 };
 
+static bool keepRunning = true;
+void intHandler(int sig) {
+    keepRunning = false;
+}
 
+static int motorCenter = (MOTOR_ESC_MAX + MOTOR_ESC_MIN) / 2;
+static int motorRange = (MOTOR_ESC_MAX - MOTOR_ESC_MIN) / 2;
+static int motorDeadZone = (MOTOR_DEADZONE / 100.0) * (MOTOR_ESC_MAX - MOTOR_ESC_MIN) / 2;
+//Set motor speed
+void setSpeed(int pi, short motorPerc) {
+    int pulseWidth = motorCenter;
+
+    if (motorPerc > 100)
+        motorPerc = 100;
+    
+    if (motorPerc < 0)
+        pulseWidth = 0;
+    else if (motorPerc > 50)
+        pulseWidth += motorDeadZone + (motorRange - motorDeadZone) * (motorPerc - 50) / 50.0;
+    else if (motorPerc < 50)
+        pulseWidth -= motorDeadZone + (motorRange - motorDeadZone) * (50 - motorPerc) / 50.0;
+    
+    set_servo_pulsewidth(pi, MOTOR_ESC, pulseWidth);
+}
+
+//Set steering angle
+void setSteering(int pi, short steeringPerc) {
+    int pulseWidth = 0;
+    
+    if (steeringPerc > 100)
+        steeringPerc = 100;
+    if (steeringPerc >= 0)
+        pulseWidth = SERVO_STEERING_MIN + ((SERVO_STEERING_MAX - SERVO_STEERING_MIN) * steeringPerc / 100);
+    
+    set_servo_pulsewidth(pi, SERVO_STEERING, pulseWidth);
+}
+
+void stopCar(int pi) {
+    setSpeed(pi, -1);
+    setSteering(pi, -1);
+}
 
 int main() {
+    struct sigaction sig;
+    sig.sa_handler = intHandler;
+    sigaction(SIGINT, &sig, NULL);
+    
     struct input_event event;
-    int pi, fd;
+    int pi, fd, speedPerc, steeringPerc;
+    clock_t startTime;
+    struct controller_status controllerStat = {0,0,0,0,0};
     
     pi = pigpio_start(NULL, NULL);
-    
-    /*
-    set_PWM_frequency(pi, SERVO_STEERING, 300);
-    set_servo_pulsewidth(pi,SERVO_STEERING, 1300);
-    sleep(3);
-    set_servo_pulsewidth(pi,SERVO_STEERING, 1100);
-    sleep(3);
-    
-    set_servo_pulsewidth(pi,SERVO_STEERING, 0);
-    */
-    pigpio_stop(pi);
-    
-    
     fd = open("/dev/input/event2", O_RDONLY);
-    if (fd == -1) {
-        printf("Error opening file\n");
-        return -1;
-    }
-    
-    struct controller_status statController = {0,0,0,0,0,0};
     
     bool paused = false;
-    while (!paused) {
-        read(fd, &event, sizeof(struct input_event));
+    while (keepRunning) {
+        if (fd == -1) {
+            printf("Controller Not Found!\n");
+            startTime = time(NULL);
+            while (keepRunning && fd == -1 && time(NULL) - startTime < 20) {
+                fd = open("/dev/input/event2", O_RDONLY);
+                sleep(1);
+            }
+            continue;
+        }
+        if (read(fd, &event, sizeof(struct input_event)) != sizeof(struct input_event)) {
+            printf("Controller Disconnected!\n");
+            close(fd);
+            fd = open("/dev/input/event2", O_RDONLY);
+            continue;
+        }
+        
         if (event.type == EV_KEY) {
-            if (event.code == EV_KEY_OPT) {
-                if (event.value == 1) {
-                    paused = true;
-                    printf("PAUSE TOGGLE\n");
-                }
-            }
-            else if (event.code == EV_KEY_R1) {
-                if (event.value == 1)
-                    printf("SHOOT\n");
-            }
+            if (event.code == EV_KEY_OPT)
+                controllerStat.opt = event.value;
+            else if (event.code == EV_KEY_R1)
+                controllerStat.r1 = event.value;
         }
         else if (event.type == EV_ABS) {
             if (event.code == EV_ABS_L2)
-                printf("L2: %u\n", event.value);
+                controllerStat.l2_pos = event.value;
             else if (event.code == EV_ABS_R2)
-                printf("R2: %u\n", event.value);
-            
+                controllerStat.r2_pos = event.value;
             else if (event.code == EV_ABS_RJS_X)
-                printf("RJS_X: %u\n", event.value);
+                controllerStat.js_x = event.value;
         }
+        
+        if (controllerStat.opt == 1) {
+            //Reset option status to 0 to prevent multiple calls if held
+            controllerStat.opt = 0;
+            paused = !paused;
+            if (paused) {
+                stopCar(pi);
+                printf("Paused!\n");
+            }
+            continue;
+        }
+        
+        if (paused)
+            continue;
+        
+        steeringPerc = 50;
+        if (controllerStat.js_x < 121 || controllerStat.js_x > 131)
+            steeringPerc = (controllerStat.js_x / 255.0) * 100;
+        setSteering(pi, steeringPerc);
+        
+        speedPerc = 50;
+        if (controllerStat.r2_pos > 0)
+            speedPerc += (controllerStat.r2_pos / 255.0) * 50;
+        if (controllerStat.l2_pos > 0)
+            speedPerc -= (controllerStat.l2_pos / 255.0) * 50;
+        setSpeed(pi, speedPerc);
+        
+        if (event.code == EV_KEY_OPT || event.code == EV_KEY_R1 || event.code == EV_ABS_L2 || event.code == EV_ABS_R2 || event.code == EV_ABS_RJS_X)
+            printf("shoot: %u | setSpeed: %u | setSteering: %u\n", controllerStat.r1, speedPerc, steeringPerc);
     }
     
+    printf("\nExiting!\n");
+    stopCar(pi);
+    pigpio_stop(pi);
     close(fd);
     
     return 0;
